e608fd74266d8f5d210aad2653b41bf373b076cf
Zaphira <laila.westphal@laposte.net>
2018-10-02 15:23:01 +0200

    60s timer updated: print fct improved

--- lcd/lcdScreen/lcdScreen.ino
+++ lcd/lcdScreen/lcdScreen.ino
@@ -1,48 +1,64 @@
 #include "LiquidCrystal.h"
 
-LiquidCrystal lcd(7,8,9,3,6,4);
-int counter = 60;
+LiquidCrystal lcd(3,4,6,7,8,9);
+int timerStart=11;
+int counter=timerStart;
+int score=0;
+
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16,2);
-
-
-
-}
-
-void timer(){ 
   lcd.setCursor(0,0);
-    lcd.print("Let's go!");
-    delay(3000);
-    lcd.clear();
-    for(counter=60;counter>=0;counter--){
-      lcd.print("Time:");
-      lcd.setCursor(0,1);
-      lcd.print(counter);
-      delay(1000);
-      lcd.clear();
-      if (counter ==0){
-    lcd.setCursor(0,1);
-    lcd.print("Time's up!");
-    delay(1000);
-    lcd.clear();
-    }
-  }
+  lcd.print("Let's go!");
+  delay(3000);
 }
 
-void printScore(int score){
+
+void timerSetup(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Let's go!");
+  delay(3000);
+  }
+
+int timer(){ 
+counter--;
+return counter;
+}
+
+
+void printTimeScore(int t, int s){
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("Time:");
+  lcd.setCursor(0,1);
+  lcd.print(t);
+
   lcd.setCursor(8,0);
   lcd.print("Score:");
   lcd.setCursor(8,1);
-  lcd.print(score);
-}
+  lcd.print(s);
+  }
+
+  void printAndRestart(int t, int s){
+    if(t==-1){
+      lcd.clear();
+      lcd.print("Time's up!");
+      counter=timerStart;
+      delay(3000);
+      timerSetup();
+    }else if(t>=0){
+      printTimeScore(t,s);
+    }
+  }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  timer();
-
+  int t=timer();
+  printAndRestart(t,score);
   delay(1000);
 
 }
